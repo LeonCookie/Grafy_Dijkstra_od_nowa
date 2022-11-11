dd045a0cf5cfd64190a6507aa2046661b259a05e
LeonCookie <LeonGdynia1105@gmail.com>
2022-11-11 21:04:21 +0100

    aplikacja z grafami od nowa/ wersja 2.0

---
+++ app/src/main/java/com/example/grafy_dijkstra_od_nowa/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.grafy_dijkstra_od_nowa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
